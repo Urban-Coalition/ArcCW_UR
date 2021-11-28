072ccda3c539ee13fe7d10aecee0ed2d4513df8c
Partexedd <arnaumorant@hotmail.com>
2021-11-28 20:54:30 +0100

    slide moves faster

--- lua/weapons/arccw_ur_deagle.lua
+++ lua/weapons/arccw_ur_deagle.lua
@@ -157,7 +157,7 @@ SWEP.ActiveAng = Angle(0, 0, -2)
 SWEP.CustomizePos = Vector(-1, -2, 1)
 SWEP.CustomizeAng = Angle(0, 0, 0)
 
-SWEP.CrouchPos = Vector(-2, -2, 0)
+SWEP.CrouchPos = Vector(-2, 0, 0)
 SWEP.CrouchAng = Angle(0, 0, -8)
 
 SWEP.MirrorVMWM = true
