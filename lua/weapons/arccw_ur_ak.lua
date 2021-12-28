5c18dbd574b51d227f2a052a93e1fd8c711ba935
Sninctbur <hedpub@protonmail.com>
2021-12-27 20:01:07 -0600

    Reorganized AK att structure for UR

--- lua/weapons/arccw_ur_ak.lua
+++ lua/weapons/arccw_ur_ak.lua
@@ -3,7 +3,7 @@ SWEP.Spawnable = true -- this obviously has to be set to true
 SWEP.Category = "ArcCW - Urban Coalition" -- edit this if you like
 SWEP.AdminOnly = false
 
-SWEP.PrintName = "AKM"
+SWEP.PrintName = "AK-47"
 SWEP.Trivia_Class = "Assault Rifle"
 SWEP.Trivia_Desc = "One of the first assault rifles, prized around the world to this day for its cheap cost, lax maintenance, and infallible reliability under harsh conditions. Consequently, a fifth of all small arms in existence can be traced to this design. The default pattern is well-rounded, but recoils harder than other weapons of its class."
 SWEP.Trivia_Manufacturer = "Izhmash"
@@ -118,11 +118,6 @@ SWEP.ShootSound = path .. "fire_auto_1.ogg", path .. "fire_auto_2.ogg"
 SWEP.DistantShootSound = "weapons/arccw_ur/ak/fire_dist.ogg"
 SWEP.ShootPitchVariation = 0
 
-SWEP.MeleeSwingSound = "arccw_go/m249/m249_draw.wav"
-SWEP.MeleeMissSound = "weapons/iceaxe/iceaxe_swing1.wav"
-SWEP.MeleeHitSound = "arccw_go/knife/knife_hitwall1.wav"
-SWEP.MeleeHitNPCSound = "physics/body/body_medium_break2.wav"
-
 SWEP.MuzzleEffect = "muzzleflash_ak47"
 SWEP.ShellModel = "models/shells/shell_556.mdl"
 SWEP.ShellPitch = 90
@@ -164,154 +159,7 @@ SWEP.CustomizeAng = Angle(5, 30, 30)
 SWEP.BarrelLength = 24
 
 SWEP.AttachmentElements = {
-    ["sidemount"] = {
-        VMBodygroups = {{ind = 1, bg = 1}},
-    },
-    ["ubrms"] = {
-        VMBodygroups = {{ind = 7, bg = 1}},
-    },
-    ["no_fh"] = {
-        VMBodygroups = {{ind = 6, bg = 0}},
-    },
-    ["go_ak_barrel_short"] = {
-        VMBodygroups = {
-            {ind = 3, bg = 1},
-            {ind = 6, bg = 1},
-        },
-        AttPosMods = {
-            [5] = {
-                vpos = Vector(0, -3.4, 15.5),
-            }
-        },
-        -- Override_IronSightStruct = false
-        Override_IronSightStruct = {
-            Pos = Vector(-5.03, -10, 1.25),
-            Ang = Angle(-0.9, 0.075, -2.412),
-            Magnification = 1.1,
-            CrosshairInSights = false
-        }
-    },
-    ["go_ak_barrel_long"] = {
-        VMBodygroups = {
-            {ind = 3, bg = 2},
-        },
-        AttPosMods = {
-            [5] = {
-                vpos = Vector(0, -3.4, 32),
-            }
-        },
-        -- Override_IronSightStruct = {
-        --     Pos = Vector(-5.03, -10, 1.1),
-        --     Ang = Angle(0.16, 0.125, -2.412),
-        --     Magnification = 1.1,
-        --     CrosshairInSights = false
-        -- }
-    },
-    ["go_ak_barrel_tac"] = {
-        VMBodygroups = {
-            {ind = 3, bg = 3},
-            {ind = 7, bg = 0}
-        },
-        AttPosMods = {
-            [2] = {
-                vpos = Vector(0, -2.25, 13),
-            }
-        }
-    },
-    ["go_ak_grip_poly"] = {
-        VMBodygroups = {
-            {ind = 4, bg = 1},
-        },
-    },
-    ["go_stock_none"] = {
-        VMBodygroups = {
-            {ind = 5, bg = 1},
-        },
-    },
-    ["go_stock"] = {
-        VMBodygroups = {
-            {ind = 5, bg = 1},
-        },
-        VMElements = {
-            {
-                Model = "models/weapons/arccw_go/atts/stock_buftube.mdl",
-                Bone = "v_weapon.ak47_Parent",
-                Offset = {
-                    pos = Vector(0, -2.75, -1.5),
-                    ang = Angle(90, 0, -90),
-                },
-            }
-        },
-    },
-    ["go_ak_stock_skeleton"] = {
-        VMBodygroups = {
-            {ind = 5, bg = 2},
-        },
-    },
-    ["go_ak_stock_basilisk"] = {
-        VMBodygroups = {
-            {ind = 5, bg = 3},
-        },
-    },
-    ["go_ak_stock_contractor"] = {
-        VMBodygroups = {
-            {ind = 5, bg = 4},
-        },
-    },
-    ["go_ak_stock_heavy"] = {
-        VMBodygroups = {
-            {ind = 5, bg = 5},
-        },
-    },
-    ["go_ak_mag_30"] = {
-        VMBodygroups = {
-            {ind = 2, bg = 1},
-        },
-    },
-    ["go_ak_mag_30_545"] = {
-        NameChange = "AK-74",
-        VMBodygroups = {
-            {ind = 2, bg = 2},
-        },
-    },
-    ["go_ak_mag_10"] = {
-        VMBodygroups = {
-            {ind = 2, bg = 3},
-        },
-    },
-    ["go_ak_mag_10_steel"] = {
-        VMBodygroups = {
-            {ind = 2, bg = 4},
-        },
-    },
-    ["go_ak_mag_40"] = {
-        VMBodygroups = {
-            {ind = 2, bg = 5},
-        },
-    },
-    ["go_ak_mag_40_steel"] = {
-        VMBodygroups = {
-            {ind = 2, bg = 6},
-        },
-    },
-    ["go_ak_mag_45_545"] = {
-        NameChange = "AK-74",
-        VMBodygroups = {
-            {ind = 2, bg = 7},
-        },
-    },
-    ["go_ak_mag_60_545"] = {
-        NameChange = "AK-74",
-        VMBodygroups = {
-            {ind = 2, bg = 8},
-        },
-    },
-    ["go_ak_mag_15_545"] = {
-        NameChange = "AK-74",
-        VMBodygroups = {
-            {ind = 2, bg = 9},
-        },
-    },
+    
 }
 
 SWEP.ExtraSightDist = 10
@@ -327,86 +175,139 @@ SWEP.MirrorVMWM = true
 SWEP.Attachments = {
     {
         PrintName = "Optic",
-        Slot = {"optic_lp", "optic"},
-        Bone = "v_weapon.ak47_Parent",
         DefaultAttName = "Iron Sights",
-        Offset = {
-            vpos = Vector(0.15, -5.3, 3),
-            vang = Angle(90, 0, -90),
-        },
-        InstalledEles = {"sidemount"},
-        CorrectiveAng = Angle(-1.25, 0, 0)
-    },
-    {
-        PrintName = "Underbarrel",
-        Slot = {"foregrip", "ubgl"},
+        InstalledEles = {"upper_flat"},
+        Slot = {"optic","sniper_optic","ur_ak_optic"},
         Bone = "tag_weapon",
         Offset = {
-            vpos = Vector(0, 10, 1.5),
-            vang = Angle(0, -90, 0),
+            vpos = Vector(0, -1.75, 3),
+            vang = Angle(90, 0, -90),
         },
-        -- InstalledEles = {"ubrms"},
-    },
-    {
-        PrintName = "Tactical",
-        Slot = "tac",
-        Bone = "v_weapon.ak47_Parent",
-        Offset = {
-            vpos = Vector(1.24, -4, 3),
-            vang = Angle(90, 0, 0),
+        VMScale = Vector(1, 1, 1),
+        ExtraSightDist = 8,
+        SlideAmount = {
+            vmin = Vector(0, -1.75, 3-2),
+            vmax = Vector(0, -1.75, 3+2),
         },
-        InstalledEles = {"sidemount"},
     },
     {
         PrintName = "Barrel",
-        Slot = "go_ak47_barrel",
-        DefaultAttName = "415mm Standard Barrel"
+        DefaultAttName = "16\" Standard Barrel",
+        DefaultAttIcon = Material("entities/att/acwatt_ud_m16_barrel_default.png", "smooth mips"),
+        Slot = "ur_ak_barrel",
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(2.8, -4.2, -11.5),
+            vang = Angle(90, 0, -90),
+        },
+        ExcludeFlags = {"m16_usas"},
     },
     {
         PrintName = "Muzzle",
         DefaultAttName = "Standard Muzzle",
-        Slot = "muzzle",
-        Bone = "v_weapon.ak47_Parent",
+        Slot = {"muzzle","ur_ak_muzzle"},
+        Bone = "tag_weapon",
+        VMScale = Vector(1.25, 1.25, 1.25),
+        WMScale = VMScale,
         Offset = {
-            vpos = Vector(0, -3.4, 25),
+            vpos = Vector(0.025, -0.05, 30.75),
             vang = Angle(90, 0, -90),
         },
-        InstalledEles = {"no_fh"}
+        ExcludeFlags = {"sd", "m16_usas", "m16_stub"},
     },
     {
-        PrintName = "Magazine",
-        Slot = {"go_ammo_ak47", "go_ammo_ak74"},
-        DefaultAttName = "30-Round 7.62mm Steel"
+        PrintName = "Receiver",
+        DefaultAttName = "Standard Receiver",
+        DefaultAttIcon = Material("entities/att/acwatt_ud_m16_receiver_default.png", "smooth mips"),
+        Slot = {"ur_ak_receiver"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(2.8, -4.2, -11.5),
+            vang = Angle(90, 0, -90),
+        },
+    },
+    {
+        PrintName = "Underbarrel",
+        Slot = {"foregrip","ubgl"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(0, 1.25, 12),
+            vang = Angle(90, 0, -90),
+        },
+        InstalledEles = {"rail_fg"},
+        ExcludeFlags = {"m16_lmg", "m16_stub"}, 
+        SlideAmount = {
+            vmin = Vector(0, 1.25, 12.5),
+            vmax = Vector(0, 1.25, 9),
+        },
+    },
+    {
+        PrintName = "Tactical",
+        Slot = {"tac"},
+        Bone = "tag_weapon",
+        Offset = {
+            vpos = Vector(0, 0.8, 27),
+            vang = Angle(90, 0, -90),
+        },
+        GivesFlags = {"tac"},
+        --InstalledEles = {"ud_m16_clamp_fullsize"}
+    },
+    {
+        PrintName = "Grip Type",
+        Slot = {"ur_ak_grip"},
+        DefaultAttName = "Standard Grip",
+        DefaultAttIcon = Material("entities/att/acwatt_ud_m16_grip_default.png", "smooth mips"),
+        ExcludeFlags = {"m16_adar"}
     },
     {
         PrintName = "Stock",
-        Slot = {"go_ak47_stock", "go_stock_none", "go_stock", "go_ak47_stock_heavy"},
-        DefaultAttName = "Standard Stock",
-        Bone = "v_weapon.ak47_Parent",
-        Offset = {
-            vpos = Vector(0, -2.75, -1.5),
-            vang = Angle(90, 0, -90),
-        },
+        Slot = {"ur_ak_stock"},
+        DefaultAttName = "Full Stock",
+        DefaultAttIcon = Material("entities/att/acwatt_ud_m16_stock_default.png", "smooth mips"),
+        ExcludeFlags = {"m16_usas"},
+    },
+    {
+        PrintName = "Magazine",
+        Slot = {"ur_ak_mag"},
+        DefaultAttName = "30-Round Mag",
+        DefaultAttIcon = Material("entities/att/acwatt_ud_m16_mag_30.png", "smooth mips"),
     },
     {
         PrintName = "Ammo Type",
-        Slot = "go_ammo",
-        DefaultAttName = "Standard Ammo"
+        DefaultAttName = "\"FMJ\" Full Metal Jacket",
+        Slot = "uc_ammo",
     },
     {
-        PrintName = "Perk",
-        Slot = "go_perk"
+        PrintName = "Powder Load",
+        Slot = "uc_powder",
+        DefaultAttName = "Standard Load"
+    },
+    {
+        PrintName = "Training Package",
+        Slot = "uc_tp",
+        DefaultAttName = "Basic Training"
+    },
+    {
+        PrintName = "Internals",
+        Slot = "uc_fg", -- Fire group
+        DefaultAttName = "Standard Internals"
     },
     {
         PrintName = "Charm",
-        Slot = "charm",
+        Slot = {"charm", "fml_charm"},
         FreeSlot = true,
-        Bone = "v_weapon.ak47_Parent", -- relevant bone any attachments will be mostly referring to
+        Bone = "tag_weapon",
         Offset = {
-            vpos = Vector(0.6, -2.75, 6), -- offset that the attachment will be relative to the bone
+            vpos = Vector(0.6, 1, 5),
             vang = Angle(90, 0, -90),
         },
     },
+    {
+        PrintName = "Dust Cover",
+        DefaultAttName = "Standard Dust Cover",
+        Slot = {"ur_ak_dustcover"},
+        FreeSlot = true,
+    }
 }
 
 function SWEP:Hook_TranslateAnimation(anim)
