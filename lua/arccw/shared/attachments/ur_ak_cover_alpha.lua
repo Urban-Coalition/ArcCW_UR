a920a9f27631a0e1f03459396d12e4ccae0d7f4d
Sninctbur <hedpub@protonmail.com>
2021-12-30 02:18:31 -0600

    Un-memed alpha cover
    Darky does a little trolling

--- lua/arccw/shared/attachments/ur_ak_cover_alpha.lua
+++ lua/arccw/shared/attachments/ur_ak_cover_alpha.lua
@@ -1,16 +1,14 @@
 att.PrintName = "Alpha AK Dust Cover"
 att.AbbrevName = "Alpha Dust Cover"
 att.Icon = nil -- todo
-att.Description = "nigga this will make you miss every shot the dust cover isn't mounted stable it shaking you need to use dovetail mount       Alternative US-made dust cover with an upper picattiny rail."
+att.Description = "Alternative US-made dust cover with an upper picattiny rail."
 att.Slot = {"ur_ak_cover"}
 att.AutoStats = true
 
 att.Desc_Neutrals = {
-    -- "uc.cosmetic",
+    "uc.cosmetic",
     "Optics become mounted on the upper rail instead of a rail adapter. (NYI)"
 }
-
-att.Mult_AccuracyMOA = -222
 
 att.ActivateElements = {"cover_alpha"}
 att.GivesFlags = {"cover_rail"}
