4c55572aa4f45dd4250b432024a2c4823d1d1dfb
Sninctbur <hedpub@protonmail.com>
2021-12-31 02:52:47 -0600

    Alpha stock nerf

--- lua/arccw/shared/attachments/ur_ak_stock_alpha.lua
+++ lua/arccw/shared/attachments/ur_ak_stock_alpha.lua
@@ -7,9 +7,9 @@ att.AutoStats = true
 att.SortOrder = 2
 
 att.Mult_Recoil = 0.85
-att.Mult_RecoilSide = 1.3
+att.Mult_RecoilSide = 1.35
 att.Mult_Sway = 0.7
 att.Mult_SightTime = 0.8
-att.Mult_SpeedMult = 1.15
+att.Mult_SpeedMult = 1.1
 
 att.ActivateElements = {"stock_alpha"}
