c3b9c464ec8d3b9032f97b01286c86917c15ecff
Sninctbur <hedpub@protonmail.com>
2022-01-02 18:43:29 -0600

    Fixed unplayable dong description

--- lua/arccw/shared/attachments/ur_ak_barrel_dong.lua
+++ lua/arccw/shared/attachments/ur_ak_barrel_dong.lua
@@ -3,7 +3,7 @@ att.AbbrevName = "Dong Foregrip"
 att.Icon = nil -- todo
 att.Description = "Romanian lower handguard design, shaped into an integrated foregrip. Functions like a Vertical Foregrip with slightly less added weight."
 att.Slot = {"ur_ak_ub"}
-att.Desc_Cons = {"uc.noubs"}
+--att.Desc_Cons = {"uc.noubs"}
 att.AutoStats = true
 
 att.SortOrder = 16
