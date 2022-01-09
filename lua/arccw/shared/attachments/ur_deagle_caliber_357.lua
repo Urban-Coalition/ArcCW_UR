4aa9eb4716a72d6b6538c829acc59bd519cd14d5
rzen1th <61303091+rzen1th@users.noreply.github.com>
2022-01-09 03:21:11 -0600

    357 firing sounds

--- lua/arccw/shared/attachments/ur_deagle_caliber_357.lua
+++ lua/arccw/shared/attachments/ur_deagle_caliber_357.lua
@@ -6,7 +6,19 @@ att.Description = "A more practical caliber with higher capacity magazines and a
 if !GetConVar("arccw_truenames"):GetBool() then
     att.PrintName = "Predator .357 Magnum Conversion"
 end
--- Todo: alt shoot sound
+
+att.Hook_GetShootSound = function(wep, sound)
+    if wep:GetBuff_Override("Silencer") then
+        return "weapons/arccw_ur/deagle/fire_supp_10.ogg" -- Placeholder
+    else
+        return {"weapons/arccw_ur/deagle/fire_357_1.ogg", "weapons/arccw_ur/deagle/fire_357_2.ogg"} -- Not Placeholder
+    end
+end
+
+--[[att.Hook_GetDistantShootSound = function(wep, distancesound)
+    if distancesound == wep.DistantShootSound then
+        return "weapons/arccw_ur/deagle/fire_dist_10.ogg" end
+end]]
 
 att.AutoStats = true
 att.Slot = "ur_deagle_caliber"
