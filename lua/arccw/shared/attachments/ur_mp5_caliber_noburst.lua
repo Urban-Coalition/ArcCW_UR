ea1938a20d14d5ddd1b986e64374298d876134d2
Sninctbur <hedpub@protonmail.com>
2022-02-25 16:23:34 -0600

    SEF is back

--- lua/arccw/shared/attachments/ur_mp5_caliber_noburst.lua
+++ lua/arccw/shared/attachments/ur_mp5_caliber_noburst.lua
@@ -7,7 +7,7 @@ end
 
 att.SortOrder = 201
 att.Icon = Material("entities/att/acwatt_ur_mp5_caliber.png", "smooth mips")
-att.Description = "Original receiver that lacks the Navy receiver's three-round burst fire mode."
+att.Description = "Original receiver that lacks the Navy receiver's three-round burst fire mode. Its grooved grip is more comfortable to hold."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -18,6 +18,8 @@ att.Desc_Neutrals = {
 att.Slot = "ur_mp5_caliber"
 
 att.AutoStats = true
+
+att.Mult_SightTime = .95
 
 att.Override_Firemodes_Priority = 0.5
 att.Override_Firemodes = {
@@ -31,5 +33,3 @@ att.Override_Firemodes = {
         Mode = 0
     }
 }
-
-att.Ignore = true
