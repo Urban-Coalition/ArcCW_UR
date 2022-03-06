00101c054069530948ff581c0d48008bb47a5497
Sninctbur <hedpub@protonmail.com>
2022-03-05 18:34:45 -0600

    Update ur_ak_stock_ak74m.lua

--- lua/arccw/shared/attachments/ur_ak_stock_ak74m.lua
+++ lua/arccw/shared/attachments/ur_ak_stock_ak74m.lua
@@ -1,8 +1,10 @@
 att.PrintName = "Polymer Stock"
 att.Icon = Material("entities/att/ur_ak/stock/n.png", "mips smooth")
-att.Description = [[Side-folding stock found since the 74 series' debut. The triangular structure reduces its compromise on stability, but it is still less effective than a fixed stock. Folding the stock provides massively boosted maneuverability at the cost of severe recoil.]]
+att.Description = [[Full side-folding stock. While extended, it functions similarly to a standard wood stock, albeit with less stability.
+Folding the stock boosts mobility and recoil, though not as significantly as skeletal folding stocks.
+
+Toggling this stock modifies performance accordingly.]]
 att.AutoStats = true
-att.Desc_Neutrals = "Toggling this stock modifies performance accordingly."
 att.Slot = {"ur_ak_stock"}
 
 att.SortOrder = 1
@@ -13,12 +15,8 @@ att.ExcludeFlags = {"mag_drum"}
 att.ToggleStats = {
     {
         PrintName = "Extended",
-        Mult_SightTime = 0.9,
-        Mult_HipDispersion = 0.95,
-        Mult_Recoil = 1.25,
-        Mult_RecoilSide = 1.1,
+        Mult_SightTime = 0.95,
         Mult_Sway = 1.2,
-        Mult_SightedSpeedMult = 1.05,
         
         ActivateElements = {"stock_ak74m"},
         AutoStats = true,
@@ -29,9 +27,9 @@ att.ToggleStats = {
         Mult_DrawTime = 0.9,
         Mult_HolsterTime = 0.9,
         Mult_Recoil = 1.25,
-        Mult_RecoilSide = 1.66,
-        Mult_SightedSpeedMult = 1.1,
-        Mult_ShootSpeedMult = 1.1,
+        Mult_RecoilSide = 1.75,
+        Mult_SightedSpeedMult = 1.05,
+        Mult_ShootSpeedMult = 1.05,
         Add_BarrelLength = -9,
         Mult_Sway = 2.5,
         ActivateElements = {"stock_ak74m_folded"},
