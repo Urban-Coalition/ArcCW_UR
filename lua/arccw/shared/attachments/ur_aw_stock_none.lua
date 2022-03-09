51350b4ee6b9522b852c600191082c51d37ae535
Sninctbur <hedpub@protonmail.com>
2022-03-08 21:10:44 -0600

    AW no stock nerfed

--- lua/arccw/shared/attachments/ur_aw_stock_none.lua
+++ lua/arccw/shared/attachments/ur_aw_stock_none.lua
@@ -8,13 +8,13 @@ att.SortOrder = -1
 att.Mult_SightTime = 0.5
 att.Mult_DrawTime = 0.6
 att.Mult_HolsterTime = 0.6
-att.Mult_Recoil = 2
+att.Mult_Recoil = 2.5
 att.Mult_RecoilSide = 1.55
 att.Mult_SightedSpeedMult = 1.25
 att.Mult_SpeedMult = 1.1
 att.Mult_ShootSpeedMult = 1.15
 att.Add_BarrelLength = -9
-att.Mult_Sway = 3
+att.Mult_Sway = 3.5
 
 att.ActivateElements = {"stock_none"}
 att.GivesFlags = {"pistolgrip"}
