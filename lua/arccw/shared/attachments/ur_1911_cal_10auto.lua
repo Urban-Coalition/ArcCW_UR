d45bcb9f5b0701f474467cf82ad2cf3ac2956cc7
Sninctbur <hedpub@protonmail.com>
2022-03-28 14:58:16 -0500

    10mm is actually weaker than 45 /shrug

--- lua/arccw/shared/attachments/ur_1911_cal_10auto.lua
+++ lua/arccw/shared/attachments/ur_1911_cal_10auto.lua
@@ -11,7 +11,7 @@ end
 
 att.SortOrder = 10
 att.Icon = Material("entities/att/acwatt_ud_glock_caliber.png", "smooth mips")
-att.Description = "The FBI's preferred caliber of choice.\nSignificantly more powerful, but weapon handling suffers. Despite being more powerful, it is smaller than .45 ACP."
+att.Description = "The FBI's preferred caliber of choice.\nDespite its distinctive power, it's not as large as .45 ACP, though its damage curve is more balanced."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -25,16 +25,16 @@ att.AutoStats = true
 att.Override_Trivia_Calibre = "10mm Auto"
 -- att.Override_Trivia_Manufacturer = "Ruger"
 
-att.Mult_Damage = 1.15
-att.Mult_DamageMin = 1.15
-att.Mult_Penetration = 8 / 6
+att.Mult_Damage = 35 / 45
+att.Mult_DamageMin = 20 / 15
+att.Mult_Penetration = 8 / 9
 
-att.Mult_Recoil = 1.3
-att.Mult_RecoilSide = 1.25
-att.Mult_ReloadTime = 1.2
-att.Mult_MalfunctionMean = 0.75
+-- att.Mult_Recoil = 1.3
+-- att.Mult_RecoilSide = 1.25
+-- att.Mult_ReloadTime = 1.2
+-- att.Mult_MalfunctionMean = 0.75
 
-att.Mult_ClipSize = 1.1
+att.Mult_ClipSize = 8 / 7
 att.Override_ShellScale = 1
 
 att.Hook_GetShootSound = function(wep, sound)
