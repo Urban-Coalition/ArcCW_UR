a1bcab6eb5372d8f69967fbdbf8f6389425cb940
Sninctbur <hedpub@protonmail.com>
2022-03-30 20:50:56 -0500

    AK flavor adjustments + alpha nerf

--- lua/arccw/shared/attachments/ur_ak_barrel_alpha.lua
+++ lua/arccw/shared/attachments/ur_ak_barrel_alpha.lua
@@ -1,15 +1,15 @@
-att.PrintName = "16\" Helix Alpha Barrel"
+att.PrintName = "16\" Zenitco Barrel"
 att.Icon = Material("entities/att/ur_ak/barrel/alpha.png", "mips smooth")
-att.Description = "US production handguard featuring a sleek and lightweight design with a rail interface for additional modularity."
+att.Description = "US production handguard featuring a rail interface for additional modularity. Especially lightweight, though less comfortable to grip."
 att.Slot = {"ur_ak_barrel"}
 att.AutoStats = true
 
 --att.Desc_Neutrals = {"uc.cosmetic"} nvm
-att.SortOrder = 16
+att.SortOrder = 15.9
 
 att.Mult_Sway = 1.1
 att.Mult_SightedSpeedMult = 1.1
-att.Mult_SpeedMult = 1.1
+att.Mult_SpeedMult = 1.05
 att.Mult_Recoil = 1.075
 
 att.GivesFlags = {"ak_railedguard","nodong"}
