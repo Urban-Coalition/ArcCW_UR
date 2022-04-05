78040c05b935094dec9d93b9924204c4d16a8069
Sninctbur <hedpub@protonmail.com>
2022-04-05 01:33:42 -0500

    AKM brake rework

--- lua/arccw/shared/attachments/ur_ak_muzzle_akm.lua
+++ lua/arccw/shared/attachments/ur_ak_muzzle_akm.lua
@@ -1,15 +1,15 @@
 att.PrintName = "AKM Muzzle Brake"
 att.Icon = Material("entities/att/ur_ak/muzzle_m.png", "mips smooth")
-att.Description = "External muzzle brake unique to the AKM and some derivatives. The slant-cut design counters muzzle rise more effectively and doesn't add as much weight as a universal compensator."
+att.Description = "External muzzle brake unique to the AKM and some derivatives. The slant-cut design counters muzzle rise without adding as much weight as a universal compensator."
 att.AutoStats = true
 att.Slot = {"ur_ak_muzzle"}
 
-att.Mult_Recoil = .8
+att.Mult_Recoil = .9
 att.Mult_RecoilSide = .95
 
 att.Add_BarrelLength = 1
-att.Mult_SightTime = 1.05
-att.Mult_Sway = 1.1
+att.Mult_SightTime = 1.025
+att.Mult_Sway = 1.05
 att.Mult_ShootVol = 1.1
 
 att.SortOrder = 999
