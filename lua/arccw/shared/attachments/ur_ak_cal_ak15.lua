40a677c5b63e14674a1c6d7c0857732b8b9c30da
ColonelRedips <gadunka.m203@gmail.com>
2022-05-10 19:24:18 -0400

    Stamped .308 AK and AK15 Receiver. I need someone to implement proper name changes for both in the weapon's lua file.

---
+++ lua/arccw/shared/attachments/ur_ak_cal_ak15.lua
@@ -0,0 +1,14 @@
+att.PrintName = "AK-15 7.62x39mm Receiver"
+att.AbbrevName = "AK-15 Receiver"
+att.Icon = Material("entities/att/ur_ak/recievers/ak12.png", "mips smooth")
+att.Description = "Modern stamped receiver derived from the AK12/400 rifle. Shares the same glossy finish and rails; otherwise, it functions similarly to regular stamped AKMs."
+att.Desc_Neutrals = {
+    "uc.cosmetic"
+}
+att.Slot = {"ur_ak_cal"}
+att.AutoStats = true
+
+att.SortOrder = 10
+
+att.ActivateElements = {"reciever_ak12"}
+att.GivesFlags = {"receiver_ak12"}
