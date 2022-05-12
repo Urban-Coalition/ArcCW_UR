2edfbec3ee0703e76047d30688cff225c77e6974
ColonelRedips <gadunka.m203@gmail.com>
2022-05-11 21:42:44 -0400

    Update ur_ak_grip_saiga.lua

--- lua/arccw/shared/attachments/ur_ak_grip_saiga.lua
+++ lua/arccw/shared/attachments/ur_ak_grip_saiga.lua
@@ -6,8 +6,10 @@ att.AutoStats = true
 att.Slot = {"ur_ak_grip"}
 
 
-att.Mult_SightTime = .9
-att.Mult_RecoilSide = 1.05
+att.Mult_SightTime = 1.05
+att.Mult_RecoilSide = 0.9
+att.Mult_Recoil = 0.9
+att.Mult_SpeedMult = 0.97
 att.Mult_SightedSpeedMult = .9
 
 att.ActivateElements = {"grip_saiga"}
