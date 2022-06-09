d3a25c07474df79beb9af128a37abb336e40da07
Sninctbur <hedpub@protonmail.com>
2022-06-09 10:41:54 -0500

    the bodygroups do get a bit quirky at night

---
+++ lua/arccw/shared/attachments/ur_1911_slide_compact.lua
@@ -0,0 +1,19 @@
+att.PrintName = "Colt Officer's Model 3.5\" Barrel"
+att.AbbrevName = "3.5\" Officer Barrel"
+att.Description = "A shortened slide reduces the amount of holster to clear and further improves agility to the detriment of long-range performance and recoil control."
+
+att.AutoStats = true
+att.Slot = "ur_m1911_slide"
+att.SortOrder = 3.5
+
+att.Mult_DrawTime = 0.85
+att.Mult_HolsterTime = 0.85
+att.Add_BarrelLength = -1
+
+att.Mult_Sway = 0.75
+att.Mult_HipDispersion = 0.85
+att.Mult_SightTime = 0.85
+
+att.Mult_AccuracyMOA = 1.5
+att.Mult_Range = 0.8
+att.Mult_Recoil = 1.2
---
+++ lua/arccw/shared/attachments/ur_1911_slide_m45.lua
@@ -0,0 +1,7 @@
+att.PrintName = "M45 MEUSOC 5\" Barrel"
+att.AbbrevName = "5\" M45 Barrel"
+att.Description = "Placeholder"
+
+att.AutoStats = true
+att.Slot = "ur_m1911_slide"
+att.SortOrder = 5
