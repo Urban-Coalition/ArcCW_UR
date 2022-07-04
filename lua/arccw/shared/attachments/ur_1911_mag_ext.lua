44d612d89cea46ef6feb24b5206fa6f2b8476475
1darky <95997959+1darky@users.noreply.github.com>
2022-07-04 20:10:03 +0700

    they are 11 rnd actually

--- lua/arccw/shared/attachments/ur_1911_mag_ext.lua
+++ lua/arccw/shared/attachments/ur_1911_mag_ext.lua
@@ -1,14 +1,14 @@
-att.PrintName = "AMAS 10-Round Extended Magazine"
+att.PrintName = "AMAS 11-Round Extended Magazine"
 if !GetConVar("arccw_truenames"):GetBool() then
-    att.PrintName = "M1911 10-Round Extended Magazine"
+    att.PrintName = "M1911 11-Round Extended Magazine"
 end
-att.AbbrevName = "10-Round Extended Mag"
+att.AbbrevName = "11-Round Extended Mag"
 att.Icon = Material("entities/att/acwatt_ur_deagle_mag_10.png","mips smooth")
 att.Description = "A higher capacity magazine increases the time you can spend without reloading."
 att.Slot = "ur_m1911_mag"
 att.AutoStats = true
 
-att.Override_ClipSize = 10
+att.Override_ClipSize = 11
 att.Mult_SightTime = 1.1
 att.Mult_ReloadTime = 1.15
 att.Mult_Sway = 1.25
