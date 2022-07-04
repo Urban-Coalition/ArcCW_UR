f0b4c4e1794791abdbe67b0bd596e6eddb33598f
Sninctbur <hedpub@protonmail.com>
2022-07-04 03:08:10 -0500

    Alpha AK stock makes sense

--- lua/arccw/shared/attachments/ur_ak_stock_alpha.lua
+++ lua/arccw/shared/attachments/ur_ak_stock_alpha.lua
@@ -1,14 +1,15 @@
 att.PrintName = "Carbine Stock"
 att.Icon = Material("entities/att/ur_ak/stock/helix.png", "mips smooth")
-att.Description = "A US made adjustable lightweight polymer stock with expectable pros and cons. Reduces vertical recoil and swaying, but drastically adds horizontal recoil."
+att.Description = "US-made lightweight stock that mounts on an AR buffer tube. More maneuverable than the solid wood stock."
 att.Slot = {"ur_ak_stock"}
 att.AutoStats = true
 
 att.SortOrder = 2
 
-att.Mult_Recoil = 0.85
+att.Mult_ShootSpeedMult = 1.1
+att.Mult_SightedSpeedMult = 1.15
 att.Mult_RecoilSide = 1.5
-att.Mult_Sway = 0.7
-att.Mult_SpeedMult = 1.05
+att.Mult_Sway = 1.25
+att.Add_BarrelLength = -2
 
 att.ActivateElements = {"stock_alpha"}
