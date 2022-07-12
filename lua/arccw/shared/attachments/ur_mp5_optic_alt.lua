f095094a40218cf3a2591d1259163357f8ec4256
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-07-13 01:40:50 +1000

    MP5 Closed Irons

--- lua/arccw/shared/attachments/ur_mp5_optic_alt.lua
+++ lua/arccw/shared/attachments/ur_mp5_optic_alt.lua
@@ -1,7 +1,7 @@
-att.PrintName = "Precision Irons"
+att.PrintName = "Closed Irons"
 
 att.Icon = Material("entities/att/acwatt_ur_mp5_barrel_ris.png", "smooth mips")
-att.Description = "Alternate iron sight position with a smaller field of view, potentially increasing user precision.\nIn reality, it's all preference."
+att.Description = "Closed iron sights with a smaller field of view, potentially increasing user precision.\nIn reality, it's all preference."
 att.Desc_Neutrals = {
     "uc.cosmetic"
 }
@@ -13,3 +13,11 @@ att.SortOrder = 9999
 att.ActivateElements = {"ur_mp5_precision_irons"}
 att.ExcludeFlags = {"barrel_sword"}
 att.IgnorePickX = true
+
+att.Override_IronSightStruct = {
+     Pos = Vector(-3.043, -5.95, 0.782),
+     Ang = Angle(-0.3, -0.53, 4.148),
+     Magnification = 1,
+     SwitchToSound = "",
+     ViewModelFOV = 74,
+}
