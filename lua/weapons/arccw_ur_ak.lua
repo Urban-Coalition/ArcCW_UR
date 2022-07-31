ebfe7710f00cf5701c59c241912e24cf7544eb5e
Sninctbur <hedpub@protonmail.com>
2022-07-30 21:32:28 -0500

    AK ubs no longer merged with charms

--- lua/weapons/arccw_ur_ak.lua
+++ lua/weapons/arccw_ur_ak.lua
@@ -799,7 +799,7 @@ SWEP.Attachments = {
         },
         InstalledEles = {"rail_fg"},
         ExcludeFlags = {"ak_noubs"},
-		MergeSlots = {16},
+		--MergeSlots = {16},
     },
     {
         PrintName = "Tactical",
