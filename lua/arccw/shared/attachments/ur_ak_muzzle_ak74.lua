0a04d5bb62edbfcf700715eba8d97ce137fc0c87
Sninctbur <hedpub@protonmail.com>
2022-08-05 00:43:36 -0500

    Slant compensator exclusive to 7.62x39

--- lua/arccw/shared/attachments/ur_ak_muzzle_ak74.lua
+++ lua/arccw/shared/attachments/ur_ak_muzzle_ak74.lua
@@ -1,8 +1,14 @@
-att.PrintName = "AK-74M Compensator"
+att.PrintName = "Kalashnikov Compensator"
+
 att.Icon = Material("entities/att/ur_ak/muzzle_74m.png", "mips smooth")
 att.Description = "External compensator used since the '70s for AKs of multiple calibers. Efficiently reduces horizontal recoil and marginally reduces vertical recoil at the cost of a frontal counterweight that will inevitably raise swaying."
 att.AutoStats = true
 att.Slot = {"ur_ak_muzzle"}
+
+if !GetConVar("arccw_truenames"):GetBool() then
+    att.PrintName = "Vladimir Compensator"
+    att.Description = string.Replace(att.Description,"AKs","AVRs") -- so we don't have to change the desc twice
+end
 
 att.Mult_Recoil = .95
 att.Mult_RecoilSide = .65
