6274bdd0d3015cc5cf72bdbdfbd76597e392d6af
Sninctbur <hedpub@protonmail.com>
2022-08-11 15:43:01 -0500

    PSG1 stock -sway

--- lua/arccw/shared/attachments/ur_g3_stock_psg.lua
+++ lua/arccw/shared/attachments/ur_g3_stock_psg.lua
@@ -1,12 +1,13 @@
 att.PrintName = "PSG-1 Sniper Stock" 
 att.AbbrevName = "Sniper Stock"
-att.Description = [[Heavy stock made for the PSG-1 sniper rifle. Greatly reduces recoil at the cost of mobility.]]
+att.Description = [[Heavy-duty marksman stock made for the PSG-1 sniper rifle. Cumbersome, but very comfortable to shoot with.]]
 att.AutoStats = true
 att.Slot = {"ur_g3_stock"}
 
 att.SortOrder = 11
 
 att.Mult_Recoil = 0.85
+att.Mult_Sway = .6
 
 att.Mult_SightTime = 1.1
 att.Mult_SpeedMult = 0.8
