5df7e302a933b094e2d69ed33954150259b6fb34
Sninctbur <hedpub@protonmail.com>
2022-08-11 14:40:41 -0500

    PSG1 barrel inaccurate without optics

--- lua/arccw/shared/attachments/ur_g3_barrel_26.lua
+++ lua/arccw/shared/attachments/ur_g3_barrel_26.lua
@@ -1,6 +1,9 @@
 att.PrintName = "26\" PSG-1 Barrel"
-att.Description = "Long barrel for the PSG-1 sniper rifle. Drastically impoves accuracy at the cost of firerate."
+att.Description = "Long barrel for the PSG-1 sniper rifle. Drastically impoves accuracy at the cost of firerate. Also lacks an iron sight."
 att.Slot = "ur_g3_barrel"
+att.Desc_Cons = {
+    "uc.nofs",
+}
 att.AutoStats = true
 
 att.SortOrder = 13
@@ -13,6 +16,12 @@ att.Mult_Recoil = 0.8
 att.Mult_AccuracyMOA = 0.7
 att.Mult_Range = 1.5
 att.Mult_RPM = 0.5
+
+att.A_Hook_Add_SightsDispersion = function(wep, data)
+    if data and !wep.Attachments[1].Installed then
+        data.add = data.add + 250
+    end
+end
 
 
 att.GivesFlags = {"g3_nohg"}
