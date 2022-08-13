83291db6993036da6142b82025d2256f85569320
Fesiug <publicfesiug@outlook.com>
2022-08-13 13:29:52 -0400

    Finish the GP-25 description

--- lua/arccw/shared/attachments/uc_ubgl_gp25.lua
+++ lua/arccw/shared/attachments/uc_ubgl_gp25.lua
@@ -1,7 +1,7 @@
 att.PrintName = "GP-25 Underbarrel Grenade Launcher"
 att.AbbrevName = "GP-25 Grenade Launcher"
 att.Icon = Material("entities/att/acwatt_uc_ubgl_gp25.png", "mips smooth")
-att.Description = "Single-shot long-barreled underbarrel grenade launcher designed for the AK. The caseless"
+att.Description = "Single-shot long-barreled underbarrel grenade launcher designed for the AK. The VOG-25 caseless projectiles can be reloaded faster, but hold less propellant and explosive yield."
 
 att.SortOrder = -100000
 
