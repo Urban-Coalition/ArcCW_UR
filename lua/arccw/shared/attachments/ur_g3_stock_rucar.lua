85eda99c69a218265491177d4f68245655ec6204
Talon141 <zachlaxtonbiz@gmail.com>
2022-08-20 23:39:51 -0700

    what is bro doing??

--- lua/arccw/shared/attachments/ur_g3_stock_rucar.lua
+++ lua/arccw/shared/attachments/ur_g3_stock_rucar.lua
@@ -5,7 +5,7 @@ if GetConVar("arccw_truenames"):GetBool() then
     att.PrintName = "Magpul UBR GEN2 Stock"
 end
 
-att.Description = [[Polymer aftermarket stock for the AR-15 welded onto the cap. Improves weapon control on the move, but lacks weight.]] -- i suggest you use settings from m16's fixed-adjustable stock
+att.Description = [[AR-style buffer tube adapter fit with an adjustable aftermarket stock. Improves weapon control on the move, but lacks weight.]] -- i suggest you use settings from m16's fixed-adjustable stock
 att.AutoStats = true
 att.Slot = {"ur_g3_stock"}
 
