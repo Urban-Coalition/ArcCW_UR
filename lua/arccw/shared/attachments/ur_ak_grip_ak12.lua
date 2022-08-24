790631494f5bded3af17d928099b647596a27334
Sninctbur <hedpub@protonmail.com>
2022-08-24 17:39:08 -0400

    AK12 grip reworked

--- lua/arccw/shared/attachments/ur_ak_grip_ak12.lua
+++ lua/arccw/shared/attachments/ur_ak_grip_ak12.lua
@@ -1,12 +1,11 @@
-att.PrintName = "AK-12 Polymer Grip"
+att.PrintName = "Grooved Polymer Grip"
 att.Icon = Material("entities/att/ur_ak/grip_ak12.png", "mips smooth")
-att.Description = "Black polymer grip with an emphasis on ergonomics."
+att.Description = "Black polymer grip with an emphasis on ergonomics. Helps mitigate vertical kick, but can slide around more."
 att.AutoStats = true
 att.Slot = {"ur_ak_grip"}
 
 
-att.Mult_SightTime = .875
-att.Mult_SpeedMult = 1.025
+att.Mult_Recoil = .9
 att.Mult_RecoilSide = 1.15
 
 att.ActivateElements = {"grip_ak12"}
