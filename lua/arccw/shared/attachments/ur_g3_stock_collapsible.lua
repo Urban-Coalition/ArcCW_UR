e9b12e07a2ce58c58ecce24f8538ae1401a36f3c
Talon141 <zachlaxtonbiz@gmail.com>
2022-10-18 10:40:28 -0700

    fuck

--- lua/arccw/shared/attachments/ur_g3_stock_collapsible.lua
+++ lua/arccw/shared/attachments/ur_g3_stock_collapsible.lua
@@ -1,5 +1,10 @@
 att.PrintName = "G3 Collapsible Stock" -- I just copied the mp5 collapsible stock for the stats. --that's ok, i probably would have done the same
 att.AbbrevName = "Collapsible Stock"
+
+if !GetConVar("arccw_truenames"):GetBool() then
+    att.PrintName = "G58 Collapsible Stock"
+end
+
 att.Icon = Material("entities/att/ur_g3/stock_colap.png","smooth mips")
 att.Description = "Lightweight collapsable stock that significantly shortens the rifle when collapsed. Though sturdy for a collapsing stock, acquiring a proper cheek weld is practically impossible, and its felt recoil reduction is poor.\n\nToggling the stock modifies performance accordingly."
 att.AutoStats = true
