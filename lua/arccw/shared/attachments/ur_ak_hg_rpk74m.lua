c6ab1a40cb02c81001646e40aff1f584a2051fc4
TheOnly8Z <888888zombies@gmail.com>
2022-12-10 21:37:29 -0600

    tweak ak handguard gently

--- lua/arccw/shared/attachments/ur_ak_hg_rpk74m.lua
+++ lua/arccw/shared/attachments/ur_ak_hg_rpk74m.lua
@@ -1,13 +1,13 @@
-att.PrintName = "LMG Handguard"
+att.PrintName = "SAW Handguard"
 att.Icon = Material("entities/att/ur_ak/handguards/rpk.png", "mips smooth")
-att.Description = "Light polymer handguard with superior agility, but higher recoil from the reduced counterweight."
+att.Description = "Light polymer handguard used on the RPK-74M. Its additional grooves makes it a bit steadier to hold."
 att.Slot = {"ur_ak_hg"}
 att.AutoStats = true
 
 att.SortOrder = 16
 
-att.Mult_Sway = .9
-att.Mult_SightTime = .85
-att.Mult_Recoil = 1.2
+att.Mult_Sway = .75
+att.Mult_SightTime = .9
+att.Mult_Recoil = 1.15
 
 att.ActivateElements = {"barrel_rpk74m"}
