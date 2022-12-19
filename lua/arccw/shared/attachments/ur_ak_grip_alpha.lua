0d62ebc9ea80a371994e1b67d4e573e069372a87
TheOnly8Z <888888zombies@gmail.com>
2022-12-18 22:25:49 -0600

    simplify polymer grip

--- lua/arccw/shared/attachments/ur_ak_grip_alpha.lua
+++ lua/arccw/shared/attachments/ur_ak_grip_alpha.lua
@@ -1,13 +1,11 @@
 att.PrintName = "Helix Polymer Grip"
+att.AbbrevName = "Polymer Grip"
 att.Icon = Material("entities/att/ur_ak/grip_helix.png", "mips smooth")
-att.Description = "Rough US-made polymer grip, eases ironsight handling at the cost of a higher recoil."
+att.Description = "Rough US-made polymer grip, prodiving a bit more sighted mobility."
 att.AutoStats = true
 att.Slot = {"ur_ak_grip"}
 
-
-att.Mult_SightTime = .95
 att.Mult_Recoil = 1.1
-att.Mult_RecoilSide = 1.05
-att.Mult_SightedSpeedMult = 1.2
+att.Mult_SightedSpeedMult = 1.05
 
 att.ActivateElements = {"grip_alpha"}
