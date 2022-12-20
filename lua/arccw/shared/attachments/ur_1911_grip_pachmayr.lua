c4a20b8b52f66868d5a38494fae31470945ca61a
TheOnly8Z <888888zombies@gmail.com>
2022-12-20 15:15:45 -0600

    rework pachmayr grip

--- lua/arccw/shared/attachments/ur_1911_grip_pachmayr.lua
+++ lua/arccw/shared/attachments/ur_1911_grip_pachmayr.lua
@@ -1,11 +1,12 @@
 att.PrintName = "M1911 Pachmayr Grip"
 att.AbbrevName = "Pachmayr Grip"
 att.Icon = Material("entities/att/ur_1911/grip_pach.png","mips smooth")
-att.Description = "Aftermarket grip with finger grooves and a pleasant rosewood texture.\nBulkier overall, but the grooves allow a quick grip for a smoother draw and holster."
+att.Description = "Aftermarket grip with finger grooves and a pleasant rosewood texture.\nThe finger grooves steadies aim, but is a bit slower to draw."
 
 att.AutoStats = true
 att.Slot = "ur_m1911_grip"
 
-att.Mult_Sway = 1.2
-att.Mult_DrawTime = 0.75
-att.Mult_HolsterTime = 0.75
+att.Mult_Sway = 0.75
+att.Mult_Recoil = 0.95
+att.Mult_DrawTime = 1.25
+att.Mult_HolsterTime = 1.25
