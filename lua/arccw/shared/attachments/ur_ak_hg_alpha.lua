1b255cf1828bfbb92cd68d3e8afe878026e76ed2
Sninctbur <hedpub@protonmail.com>
2022-12-22 17:45:51 -0600

    Zenitco is not American + rebalance

--- lua/arccw/shared/attachments/ur_ak_hg_alpha.lua
+++ lua/arccw/shared/attachments/ur_ak_hg_alpha.lua
@@ -1,15 +1,19 @@
-att.PrintName = "Zenitco Handguard"
+att.PrintName = "AK Zenitco Aluminum Handguard"
+att.AbbrevName = "Aluminum Handguard"
+if !GetConVar("arccw_truenames"):GetBool() then
+    att.PrintName = "KF Aluminum Handguard"
+end
 att.Icon = Material("entities/att/ur_ak/handguards/alpha.png", "mips smooth")
-att.Description = "US production handguard featuring a rail interface for additional modularity. Especially lightweight, though less comfortable to grip."
+att.Description = "Fancy Russian-made handguard featuring a rail interface for additional modularity. Especially lightweight, though less comfortable to grip."
 att.Slot = {"ur_ak_hg"}
 att.AutoStats = true
 
 --att.Desc_Neutrals = {"uc.cosmetic"} nvm
 att.SortOrder = 15.9
 
-att.Mult_Sway = 1.1
-att.Mult_SightedSpeedMult = 1.1
-att.Mult_SpeedMult = 1.05
+att.Mult_Sway = 1.2
+att.Mult_SightedSpeedMult = 1.2
+-- att.Mult_SpeedMult = 1.05
 att.Mult_Recoil = 1.075
 
 att.GivesFlags = {"ak_railedguard","nodong"}
