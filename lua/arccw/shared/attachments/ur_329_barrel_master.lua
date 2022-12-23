813837056331636e1d82f759038dbd5c5aec3cc9
Sninctbur <hedpub@protonmail.com>
2022-12-23 16:16:33 -0600

    Balanced big boy barrel

--- lua/arccw/shared/attachments/ur_329_barrel_master.lua
+++ lua/arccw/shared/attachments/ur_329_barrel_master.lua
@@ -1,17 +1,18 @@
 att.PrintName = "7\" Master Barrel"
+att.AbbrevName = "7\" Master Barrel"
 att.Icon = Material("entities/att/acwatt_ur_329_barrel_m29.png","smooth mips")
-att.Description = "Super long barrel."
+att.Description = "Huge, imposing barrel with extra frontal weight. Hard to keep steady, but hones accuracy even further."
 att.Slot = "ur_329_barrel"
 att.AutoStats = true
 att.SortOrder = 7
 
-att.Mult_AccuracyMOA = 0.7
-att.Mult_Range = 1.5
-att.Mult_Recoil = 0.85
-att.Mult_SightTime = 1.25
-att.Mult_Sway = 1.15
-att.Mult_HipDispersion = 1.1
-att.Mult_PhysBulletMuzzleVelocity = 1.15
+att.Mult_AccuracyMOA = 0.5
+att.Mult_Range = 1.75
+att.Mult_Recoil = 0.7
+att.Mult_SightTime = 1.4
+att.Mult_Sway = 1.35
+att.Mult_HipDispersion = 1.15
+att.Mult_PhysBulletMuzzleVelocity = 1.4
 
 att.Add_BarrelLength = 5
 
