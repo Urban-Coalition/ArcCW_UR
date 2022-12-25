950fa8e046153cf7ba799d8ec6bd7fff7405724a
Sninctbur <hedpub@protonmail.com>
2022-12-24 21:40:14 -0600

    .44 Special uses SW586 sounds

--- lua/arccw/shared/attachments/ur_329_caliber_44special.lua
+++ lua/arccw/shared/attachments/ur_329_caliber_44special.lua
@@ -16,18 +16,34 @@ att.Override_PhysBulletMuzzleVelocity = 265
 
 att.Override_Trivia_Calibre = ".44 Super"
 
--- att.Hook_GetShootSound = function(wep, sound)
---     if wep:GetBuff_Override("Silencer") then
---         return "weapons/arccw_ur/deagle/fire_supp_10.ogg" -- Placeholder
---     else
---         return {"weapons/arccw_ur/sw329/fire-01.ogg", "weapons/arccw_ur/sw329/fire-02.ogg", "weapons/arccw_ur/sw329/fire-03.ogg", "weapons/arccw_ur/sw329/fire-04.ogg", "weapons/arccw_ur/sw329/fire-05.ogg", "weapons/arccw_ur/sw329/fire-06.ogg"} -- Not Placeholder
---     end
--- end
--- 
--- att.Hook_GetDistantShootSoundOutdoors = function(wep, distancesound)
---     if wep:GetBuff_Override("Silencer") then
---         -- fallback to script
---     else
---         return {"weapons/arccw_ur/sw329/fire-dist-01.ogg", "weapons/arccw_ur/sw329/fire-dist-02.ogg", "weapons/arccw_ur/sw329/fire-dist-03.ogg", "weapons/arccw_ur/sw329/fire-dist-04.ogg", "weapons/arccw_ur/sw329/fire-dist-05.ogg", "weapons/arccw_ur/sw329/fire-dist-06.ogg"}
---     end
--- end
+local path = "weapons/arccw_ur/sw586/"
+
+att.Hook_GetShootSound = function(wep, sound)
+    if wep:GetBuff_Override("Silencer") then
+        return -- how tf did you equip a suppressor with this
+    else
+        return {
+            path .. "fire-01.ogg",
+            path .. "fire-02.ogg",
+            path .. "fire-03.ogg",
+            path .. "fire-04.ogg",
+            path .. "fire-05.ogg",
+            path .. "fire-06.ogg",
+        } -- Placeholder(?)
+    end
+end
+
+att.Hook_GetDistantShootSoundOutdoors = function(wep, distancesound)
+    if wep:GetBuff_Override("Silencer") then
+        -- fallback to script
+    else
+        return {
+            path .. "fire-dist-01.ogg",
+            path .. "fire-dist-02.ogg",
+            path .. "fire-dist-03.ogg",
+            path .. "fire-dist-04.ogg",
+            path .. "fire-dist-05.ogg",
+            path .. "fire-dist-06.ogg",
+        } -- Placeholder(?)
+    end
+end
