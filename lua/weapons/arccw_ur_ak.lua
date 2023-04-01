15d4840b1effa18161c692554c0f7caf103fe3ff
Sninctbur <hedpub@protonmail.com>
2023-04-01 13:27:45 -0400

    Fixed(?) RPK-74M name

--- lua/weapons/arccw_ur_ak.lua
+++ lua/weapons/arccw_ur_ak.lua
@@ -1051,7 +1051,7 @@ SWEP.Hook_NameChange = function(wep,name)
 
     if cal == "545" then
         wep.Trivia_Desc = descStart .. desc_545
-        if (hg == "74m" or hg == "rpk74m" or hg == "ak12") and stock == "ak74m" then
+        if (hg == "74m" or hg == "rpk74m" or hg == "ak12") and (stock == "ak74m" or barr == "rpk") then
             noN = true
             post = (fakeNames and "-76M") or "-74M"
         elseif shortBarrs[barr] then
