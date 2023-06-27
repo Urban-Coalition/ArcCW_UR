e8969d4567013bbf7dafcbe40d8b33fa749788dc
rzen1th <pierce4077m@gmail.com>
2023-06-26 21:27:25 -0500

    2bst cycle soundtable

--- lua/weapons/arccw_ur_spas12.lua
+++ lua/weapons/arccw_ur_spas12.lua
@@ -385,13 +385,13 @@ SWEP.Animations = {
     }, 
 	["cycle_2bst"] = {
         Source = {"cycle_freeman", "cycle_freeman2"},
-        ShellEjectAt = 0.2,
+        ShellEjectAt = 0.37,
         MinProgress = 0.6,
         --Time = 25 / 30,
         SoundTable = {
-            {s = path .. "forearm_back.ogg", t = 0.1},
-            {s = path1 .. "eject.ogg", t = 0.1},
-            {s = path .. "forearm_forward.ogg", t = 0.3}, -- Not temporary
+            {s = path .. "forearm_back.ogg", t = 0.3},
+            {s = path1 .. "eject.ogg", t = 0.37},
+            {s = path .. "forearm_forward.ogg", t = 0.5}, -- Not temporary
         },
     },
     ["unjam"] = {
