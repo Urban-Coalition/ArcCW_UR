a0529dd5ae0ca7ee063436d1c3431222ee970f34
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2023-07-04 20:32:43 +0700

    scarf iron head jirachi for the 60% flinch rate (i am gambling)

--- lua/weapons/arccw_ur_spas12.lua
+++ lua/weapons/arccw_ur_spas12.lua
@@ -275,7 +275,7 @@ SWEP.Hook_TranslateAnimation = function(wep,anim)
 			return "idle_empty_manual"
 		end
 		if	anim == "exit_inspect_empty" then
-			return "exit_inspect"
+			return "exit_inspect_pump_empty"
 		end
     end
 end
@@ -639,6 +639,23 @@ SWEP.Animations = {
     },
     ["exit_inspect"] = {
         Source = "inspect_exit",
+        -- time = 66 / 60,
+        LHIK = true,
+        LHIKIn = 0,
+        LHIKOut = 999, -- maybe im dumb
+        SoundTable = {
+            {s = common .. "movement-shotgun-02.ogg", t = 0.3},
+            {s = rottle, t = 0.18},
+            {s = rottle, t = 1.0},
+            {s = common .. "movement-shotgun-03.ogg", t = 1.3},
+            {s = path .. "presscheck1.ogg", t = 1.6},
+            {s = path .. "presscheck2.ogg", t = 2.1},
+            {s = rottle, t = 2.2},
+            {s = common .. "movement-shotgun-04.ogg", t = 2.25},
+        },
+    },
+    ["exit_inspect_pump_empty"] = {
+        Source = "inspect_exit_pump_empty",
         -- time = 66 / 60,
         LHIK = true,
         LHIKIn = 0,
